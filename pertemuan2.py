629a6441d2bb43d3cc087ff58dcfbf8d1e88ddb0
zanaa1314 <rng1373@gmail.com>
2026-05-24 16:25:11 +0700

    pertemuan 2
    lapak pertemuan 2

---
+++ pertemuan2.py
@@ -0,0 +1,70 @@
+# nomor 1
+print("2")
+print(2)
+
+# nomor 2 
+print(11111111)
+print(-11111111)
+
+# nomor 3 
+print(0o17)
+print(0x17)
+
+# nomor 4
+print(0.4)
+print(4.0)
+print(.4)
+print(4.)
+print(4e-1)
+print(4e0)
+
+# nomor 5
+print("Hello World")
+print('Hello World')
+
+# nomor 6 
+print(True)
+print(False)
+
+# nomor 7
+print(2 ** 3)
+print(2 ** 3.0)
+print(2.0 ** 3)
+print(2.0 ** 3.0)
+
+# nomor 8
+print(2 * 3)
+print(2 * 3.0)
+print(2.0 * 3)
+print(2.0 * 3.0)
+
+# nomor 9
+print(4 / 2)
+print(4 / 2.0)
+print(4.0 / 2)
+print(4.0 / 2.0)
+
+# nomor 10 
+print(7 // 2)
+print(7 // 2.0)
+print(8.0 // 3)
+print(8.0 // 3.0)
+
+# nomor 11 
+print(7 % 3)
+print(11 % 3.0)
+print(15.0 % 4)
+print(15.0 % 4.0)
+
+# nomor 12 
+print(+5)
+print(-5)
+print(5 + 3)
+print(5 - 3)
+
+# nomor 13 
+print(10 - (4 + 1))
+print(20 + (7 * 6))
+
+# nomor 14 
+print((2 ** 3), (4 * 5), (10 / 2), (10 // 3))
