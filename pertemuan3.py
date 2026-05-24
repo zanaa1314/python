dc8bcd7e48fc555a049c900963a05318728eb01c
zanaa1314 <rng1373@gmail.com>
2026-05-24 16:37:50 +0700

    pertemuauan 3
    laprak pertemuan 3

---
+++ pertemuan3.py
@@ -0,0 +1,97 @@
+# Nomor 1
+var = 7
+nilai = 95
+nama_mahasiswa = "Rozaan Naufal Gunawan"
+
+print(var)
+print(nilai)
+print(nama_mahasiswa)
+print(var, nilai, nama_mahasiswa)
+
+
+# Nomor 2
+Nama = "Budi"
+
+print(Nama)
+print(nama)
+
+
+# Nomor 3
+umur = 19
+
+print(umur)
+
+umur = umur + 5
+
+print(umur)
+
+
+# Nomor 4
+tabungan = 200000
+
+tabungan = tabungan - 50000
+
+print(tabungan)
+
+
+# Nomor 5
+a = 5
+
+a += 10
+
+print(a)
+
+
+# Nomor 6
+a = 3.0
+b = 4.0
+
+c = (a ** 2 + b ** 2) ** 0.5
+
+print("Nilai c =", c)
+
+
+# Nomor 7
+Ayu = 100000
+Bagus = 75000
+Citra = 125000
+
+print(Ayu, Bagus, Citra)
+
+jumlahTabungan = Ayu + Bagus + Citra
+
+print("Jumlah tabungan adalah", jumlahTabungan)
+
+
+# Nomor 8
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = miles * 1.61
+kilometers_to_miles = kilometers / 1.61
+
+print(round(miles_to_kilometers, 2))
+print(round(kilometers_to_miles, 2))
+
+
+# Nomor 9
+x = 0
+x = float(x)
+
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+
+print("y =", y)
+
+x = 1
+x = float(x)
+
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+
+print("y =", y)
+
+x = -1
+x = float(x)
+
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+
+print("y =", y)
